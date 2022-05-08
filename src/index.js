717cc9a736da5f5bb24981bc94ff308f72d6dca7
Jeremy Platt <jplatt4427@gmail.com>
2022-05-08 11:25:03 -0700

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  element = document.getElementById("create-task-form");
+  element.addEventListener('submit', function addTask(event){
+    task = document.getElementById("new-task-description").value;
+    ul = document.getElementById("tasks");
+    li = document.createElement("li");
+    delBTN = document.createElement("button");
+    delBTN.innerHTML = "Completed Task!";
+    delBTN.id = "deleteButton";
+    li.innerHTML = task;
+    console.log(task);
+    ul.appendChild(li);
+    li.appendChild(delBTN)
+    delBTN.addEventListener("click", function(){
+      this.parentElement.remove();
+    });
+    event.preventDefault();
+  });
 });
+
+
